755e57a558ed116bdd112516e8397e3fe988a89d
Kyle <berlinerkyle@gmail.com>
2024-02-14 12:00:34 -0700

    Added test field to delete all users, this will be removed for production. Also figured out the bcrypt conflict, as the password was being hashed twice.

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
@@ -7,44 +7,49 @@ require('dotenv').config();
 // The "Test" Function
 
 exports.test = (req, res, next) => {
-    console.log('HIT');
-    res.status(201).json({
-        message: 'Tested Successfully!'
-    })
+    User.deleteMany({}).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        error => {
+            res.status(400).json({
+                error
+            })
+        }
+    )
 }
 
 // The "Signup" Function
 
 exports.signup = (req, res, next) => {
-    console.log(process.env);
-    bcrypt.hash(req.body.password, 10).then(
-        (hash) => {
-            const user = new User({
+    console.log(req.body);
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    let token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_VERIFICATION_CODE,
+        { expiresIn: '2h' }
+    );
+    user.save().then(
+        () => {
+            res.status(201).json({
+                message: 'User added successfully!',
                 username: req.body.username,
-                email: req.body.email,
-                password: hash
+                userId: user._id,
+                token: token
             });
-            let token = jwt.sign(
-                { userId: user._id },
-                process.env.JWT_VERIFICATION_CODE,
-                { expiresIn: '2h' }
-            );
-            user.save().then(
-                () => {
-                    res.status(201).json({
-                        message: 'User added successfully!',
-                        username: req.body.username,
-                        userId: user._id,
-                        token: token
-                    });
-                }
-            ).catch(
-                (error) => {
-                    res.status(500).json({
-                        error: error
-                    });
-                }
-            )
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
         }
     )
 }
@@ -59,17 +64,27 @@ exports.login = (req, res, next) => {
                     error: new Error('User not found!')
                 });
             }
-            bcrypt.compare(req.body.password, user.password).then(
+            bcryptjs.compare(req.body.password, user.password).then(
                 (valid) => {
                     if (!valid) {
                         return res.status(401).json({
+                            message: "Incorrect Password!",
                             error: new Error('Incorrect password!')
                         });
                     }
 
+                    // Signing JWT
+
+                    let token = jwt.sign(
+                        { userId: user._id },
+                        process.env.JWT_VERIFICATION_CODE,
+                        { expiresIn: '2h' }
+                    );
+
                     res.status(200).json({
                         userId: user._id,
-                        username: user.username
+                        username: user.username,
+                        token: token
                     });
                 }
             ).catch(
