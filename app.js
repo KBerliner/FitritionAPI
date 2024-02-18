3798d162fd2441c75e208064fb40c61f77d49b29
Kyle <berlinerkyle@gmail.com>
2024-02-18 10:49:59 -0700

    Added the workout route to the app.

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 // Installing Dependencies
 
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
+const workoutRoutes = require("./routes/workout");
 
 app.use(express.json());
 
 // Connecting to MongoDB
 
-mongoose.connect(process.env.DATABASE_URL)
-    .then(() => {
-        console.log('successfully connected to MongoDB Atlas!');
-    })
-    .catch((error) => {
-        console.log('Unable to connect to MongoDB Atlas!');
-        console.log(error);
-    });
+mongoose
+	.connect(process.env.DATABASE_URL)
+	.then(() => {
+		console.log("successfully connected to MongoDB Atlas!");
+	})
+	.catch((error) => {
+		console.log("Unable to connect to MongoDB Atlas!");
+		console.log(error);
+	});
 
 // Header Middleware
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authroization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authroization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
 });
 
 // API Request Routing
 
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
+app.use("/api/workout", workoutRoutes);
 
 // Export
 
