985a97d9dd23960a23914503a59bc53a5e353330
Kyle <berlinerkyle@gmail.com>
2024-02-18 13:39:48 -0700

    Changed the format to store workouts. They will be unique by id and name, not by collection.

--- models/workout.js
+++ models/workout.js
@@ -4,17 +4,29 @@ const mongoose = require("mongoose");
 
 // Creating a blueprint for the "Workout" object
 
-const hikeSchema = mongoose.Schema({
+const workoutSchema = mongoose.Schema({
 	type: { type: String, required: true },
 	userId: { type: String, required: true },
 	calBurned: { type: Number },
 	date: { type: String, required: true },
 	startTime: { type: String, required: true },
 	endTime: { type: String, required: true },
+	feetClimbed: { type: Number },
+	gradesArray: { type: Array },
 	milesHiked: { type: Number },
 	elevationGain: { type: Number },
+	poundsLifted: { type: Number },
+	milesRan: { type: Number },
+	avgPace: { type: String },
+	floors: { type: Number },
+	steps: { type: Number },
+	avgHR: { type: String },
+	yardsSwam: { type: Number },
+	stroke: { type: String },
+	pool: { type: Boolean },
+	milesWalked: { type: Number },
 });
 
 // Exporting blueprint
 
-module.exports = mongoose.model("Hike", hikeSchema);
+module.exports = mongoose.model("Workout", workoutSchema);
